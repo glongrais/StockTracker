13dbb4343769db5a97ce9a0b53e873dd53df1f8f
DarkManPC <guillaume.longrais@orange.fr>
2019-08-21 12:03:10 +0200

    Initial Commit

---
+++ StocksTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksTrack
+//
+//  Created by Guillaume Longrais on 21/08/2019.
+//  Copyright © 2019 Guillaume Longrais. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksTrackTests/StocksTrackTests.swift
@@ -0,0 +1,34 @@
+//
+//  StocksTrackTests.swift
+//  StocksTrackTests
+//
+//  Created by Guillaume Longrais on 21/08/2019.
+//  Copyright © 2019 Guillaume Longrais. All rights reserved.
+//
+
+import XCTest
+@testable import StocksTrack
+
+class StocksTrackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
